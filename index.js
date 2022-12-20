42a093c5669568b536deb606dbd46f1e7c8474c1
byronrmrz <byronrmrz@gmail.com>
2022-12-20 10:03:43 -0600

    adding templates

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const { createBot } = require('whatsapp-cloud-api');
 (async () => {
     try {
       
-      const from = '108957422027938';
+      const from = '';
       const token = '';
-      const to = '50251246386';
-      const webhookVerifyToken = 'byronrmrzwebhookw';
+      const to = ''; 
+      const webhookVerifyToken = '';
   
       const bot = createBot(from, token);
   
       // Send text message, image, or pdf
       var docName = "Documento de prueba Coincaex";
-      const result = await bot.sendText(to, 'Buena tarde, Mensaje de prueba realizado con exito');
+      //const result = await bot.sendText(to, 'Buena tarde, Mensaje de prueba realizado con exito');
+      const result = await bot.sendTemplate(to, 'hello_world', 'en_us');
       const resultpdf = await bot.sendDocument(to, 
         'https://www.guiometrics.com/wp-content/uploads/2019/06/Gui%CC%81a-Whatsapp-Business-mo%CC%81vil.pdf',
         {filename:docName}
